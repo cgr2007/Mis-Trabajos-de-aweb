8a32b6d426268522f861ccdc068fc4b0d880a02a
cgr.2007 <casadogonzalezrobertoo@gmail.com>
2026-02-05 19:17:54 +0100

    Delete CSS directory

--- CSS/debug_css.py
+++
@@ -1,4 +0,0 @@
-with open(r'c:\Clase 25 26\AWEB Clase\CSS\neon_styles.css', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    for i in range(1, 12):
-        print(f"{i}: {repr(lines[i-1])}")
